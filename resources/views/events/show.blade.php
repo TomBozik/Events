a0a7cb6f1c6a112ecfe7177d50373fe6ce88b280
TomBozik <tombozik@gmail.com>
2020-08-03 14:22:38 +0200

    smaller padding

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -5,16 +5,16 @@
 	<div class="container flex flex-col mx-auto custom-min-h-full">
 		<div class="flex flex-col flex-1 bg-gray-100 md:flex-row">
 
-			<persons-list :is-admin="{{ $isAdmin }}" :person-id="{{ $person->id }}" class="order-2 pt-4 md:pt-16 md:order-1"></persons-list>
+			<persons-list :is-admin="{{ $isAdmin }}" :person-id="{{ $person->id }}" class="order-2 pt-2 md:pt-16 md:order-1"></persons-list>
 			
 			<div class="flex flex-col order-1 w-full pl-0 md:pl-16 md:order-2">
 				<event-info :is-admin="{{ $isAdmin }}" class="pt-2"></event-info>
 				<answers></answers>
 				<calendar></calendar>
-				<invitation-link url="{{ url('/events'. '/'. $event->code) }}" class="self-center hidden pt-8 text-gray-600 md:block"></invitation-link>
+				<invitation-link url="{{ url('/events'. '/'. $event->code) }}" class="self-center hidden text-gray-600 md:block"></invitation-link>
 			</div>
 
-			<invitation-link url="{{ url('/events'. '/'. $event->code) }}" class="order-3 pt-8 text-gray-600 md:hidden"></invitation-link>
+			<invitation-link url="{{ url('/events'. '/'. $event->code) }}" class="order-3 pt-2 text-gray-600 md:hidden"></invitation-link>
 
 			
 			@if (session('personCode'))
