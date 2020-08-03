fe79f3d9a3989b93d1a8b7b3ba87ed1745483ebd
TomBozik <tombozik@gmail.com>
2020-08-03 20:06:33 +0200

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    purge: {
-      content: [
-        "app/**/*.php",
-        "resources/**/*.html",
-        "resources/**/*.js",
-        "resources/**/*.jsx",
-        "resources/**/*.ts",
-        "resources/**/*.tsx",
-        "resources/**/*.php",
-        "resources/**/*.vue",
-        "resources/**/*.twig",
-      ],
-      options: {
-          // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-          whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
-      }
-    },
+    // purge: {
+    //   content: [
+    //     "app/**/*.php",
+    //     "resources/**/*.html",
+    //     "resources/**/*.js",
+    //     "resources/**/*.jsx",
+    //     "resources/**/*.ts",
+    //     "resources/**/*.tsx",
+    //     "resources/**/*.php",
+    //     "resources/**/*.vue",
+    //     "resources/**/*.twig",
+    //   ],
+    //   options: {
+    //       // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+    //       whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+    //   }
+    // },
     variants: {
       borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
     },
